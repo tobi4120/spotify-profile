3b23f4602609d45442ddc44b060dd355fe06a84c
Michael Tobis <tobi4120@mylaurier.ca>
2021-04-29 22:23:04 -0400

    server added to handle get/post requests to spotify

---
+++ server/server.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const SpotifyWebAPI = require('spotify-web-api-node')
+
+const app = express();
+app.use(cors())
+app.use(bodyParser.json())
+
+app.post('/login', (req, res) => {
+    const code = req.body.code
+    const spotifyAPI = new SpotifyWebAPI({
+        redirectUri: 'http://localhost:3000/',
+        clientId: '71a31a9286b54db9839fd01a9d059ae1',
+        clientSecret: '9357e63c9ed04557be2f8325eb07d0a4'
+    })
+
+    spotifyAPI.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        })
+    }).catch((err) => {
+        console.log(err)
+        res.sendStatus(400)
+    })
+})
+
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyAPI = new SpotifyWebAPI({
+        redirectUri: 'http://localhost:3000/',
+        clientId: '71a31a9286b54db9839fd01a9d059ae1',
+        clientSecret: '9357e63c9ed04557be2f8325eb07d0a4',
+        refreshToken
+    })
+
+    spotifyAPI
+        .refreshAccessToken()
+        .then(data => {
+            res.json({
+                accessToken: data.body.accessToken,
+                expiresIn: data.body.expiresIn,
+            })
+        })
+        .catch(err => {
+            console.log(err)
+            res.sendStatus(400)
+        })
+})
+
+app.listen(3001)
--- src/components/login.js
+++
@@ -1,9 +0,0 @@
-function Login() {
-    return (
-        <div>
-            <h1>Spotify Profile</h1>
-            <a>Log In To Spotify</a>
-        </div>
-    )
-}
-export default Login
