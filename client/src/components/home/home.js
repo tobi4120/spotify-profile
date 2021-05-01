99824cd1aa92755ba967c448f2616fc519f757cb
Michael Tobis <tobi4120@mylaurier.ca>
2021-04-30 20:50:20 -0400

    test get request to spotify api

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import useAuth from '../useAuth';
+import axios from "axios"
 
 function Home(props) {
     const accessToken = useAuth(props.code)
+
+    useEffect(async () => {
+
+        if (!accessToken) return
+        
+        const response = await axios.get("https://api.spotify.com/v1/me", 
+            { headers: {"Authorization" : `Bearer ${accessToken}`} })
+
+        console.log(response)
+    })
+
     return (
         <div>
             Home
--- client/src/components/useAuth.js
+++ client/src/components/useAuth.js
@@ -2,44 +2,50 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 
 export default function useAuth(code) {
-  const [accessToken, setAccessToken] = useState()
-  const [refreshToken, setRefreshToken] = useState()
-  const [expiresIn, setExpiresIn] = useState()
+    const [accessToken, setAccessToken] = useState()
+    const [refreshToken, setRefreshToken] = useState()
+    const [expiresIn, setExpiresIn] = useState()
 
-  useEffect(() => {
-    axios
-      .post("http://localhost:3001/login", {
-        code,
-      })
-      .then(res => {
-        setAccessToken(res.data.accessToken)
-        setRefreshToken(res.data.refreshToken)
-        setExpiresIn(res.data.expiresIn)
-        window.history.pushState({}, null, "/")
-      })
-      .catch(() => {
-        window.location = "/"
-      })
-  }, [code])
+    useEffect(async () => {
 
-  useEffect(() => {
-    if (!refreshToken || !expiresIn) return
-    const interval = setInterval(() => {
-      axios
-        .post("http://localhost:3001/refresh", {
-          refreshToken,
+        axios.post("http://localhost:3001/login", {
+            code,
         })
         .then(res => {
-          setAccessToken(res.data.accessToken)
-          setExpiresIn(res.data.expiresIn)
+            setAccessToken(res.data.accessToken)
+            setRefreshToken(res.data.refreshToken)
+            setExpiresIn(res.data.expiresIn)
+
+            // Put access token in local storage and clear code in URL
+            localStorage.setItem("token", res.data.refreshToken)
+            window.history.pushState({}, null, "/")
         })
         .catch(() => {
-          window.location = "/"
+            window.location = "/"
+            localStorage.clear();
         })
-    }, (expiresIn - 60) * 1000)
+    
+    }, [code])
 
-    return () => clearInterval(interval)
-  }, [refreshToken, expiresIn])
+    useEffect(() => {
+        if (!refreshToken || !expiresIn) return
+        const interval = setInterval(() => {
+        axios
+            .post("http://localhost:3001/refresh", {
+                refreshToken,
+            })
+            .then(res => {
+                setAccessToken(res.data.accessToken)
+                setExpiresIn(res.data.expiresIn)
+            })
+            .catch(() => {
+                window.location = "/"
+                localStorage.clear();
+            })
+        }, (expiresIn - 60) * 1000)
+
+        return () => clearInterval(interval)
+    }, [refreshToken, expiresIn])
 
   return accessToken
 }
