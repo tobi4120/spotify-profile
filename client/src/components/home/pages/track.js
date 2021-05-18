86b37457f0193209d70d7ac3e3450d09f4b6215f
Michael Tobis <tobi4120@mylaurier.ca>
2021-05-18 00:25:35 -0400

    added a chart to each track page to show the characteristics of that track

--- client/src/components/home/pages/track.js
+++ client/src/components/home/pages/track.js
@@ -2,12 +2,15 @@ import React, { useEffect, useState } from "react";
 import useAuth from "../../useAuth";
 import Loader from "../loader";
 import axios from "axios";
+import { Bar } from "react-chartjs-2";
 
 export default function Track(props) {
     const accessToken = useAuth(props.code)
     const [isLoading, set_isLoading] = useState(true)
     const [track, set_track] = useState({})
     const [error, set_error] = useState(false)
+
+    const keys = {0: 'C', 1: 'D♭', 2: 'D', 3: 'E♭', 4: 'E', 5: 'F', 6: 'G♭', 7: 'G', 8: 'A♭', 9: 'A', 10: 'B♭', 11: 'B'}
 
     useEffect(() => {
         if (!accessToken) return
@@ -44,11 +47,22 @@ export default function Track(props) {
             set_isLoading(false)
         })
 
+        // Parse through features response and put the necessary data in an array called features
+        const features = { 
+            "acousticness": features_response.data.acousticness, 
+            "danceability":  features_response.data.danceability,
+            "energy":  features_response.data.energy,
+            "instrumentalness":  features_response.data.instrumentalness,
+            "liveness":  features_response.data.liveness,
+            "speechiness":  features_response.data.speechiness,
+            "valence":  features_response.data.valence,
+        }
+
         // If no track, then return
         if (!general_response) return
         
         // Put responses in track variable
-        set_track({...track, general: general_response.data, analysis: analysis_response.data, features: features_response.data })
+        set_track({...track, general: general_response.data, analysis: analysis_response.data, features: features })
 
         set_isLoading(false)
     }
@@ -81,7 +95,7 @@ export default function Track(props) {
                     <tbody>
                         <tr>
                             <td>
-                                <p>
+                                <p className="track-page__track-data">
                                     {/* Track minutes */}
                                     {Math.floor(track.analysis.track.duration / 60)}:
 
@@ -90,40 +104,82 @@ export default function Track(props) {
                                         minimumIntegerDigits: 2,
                                     })}
                                 </p>
-                                <p>Duration</p>
+                                <p className="track-page__label">Duration</p>
                             </td>
                             <td>
-                                
+                                <p className="track-page__track-data">{keys[`${track.analysis.track.key}`]}</p>
+                                <p className="track-page__label">Key</p>
                             </td>
                             <td>
-                                
+                                <p className="track-page__track-data">{track.analysis.track.mode === 1? 'Major': 'Minor'}</p>
+                                <p className="track-page__label">Modality</p>
                             </td>
                             <td>
-                                
+                                <p className="track-page__track-data">{track.analysis.track.time_signature}</p>
+                                <p className="track-page__label">Time Signature</p>
                             </td>
                             <td>
-                                
+                                <p className="track-page__track-data">{Math.round(track.analysis.track.tempo)}</p>
+                                <p className="track-page__label">Tempo (BPM)</p>
                             </td>
                         </tr>
                         <tr>
                             <td>
-
+                                <p className="track-page__track-data">{track.general.popularity}%</p>
+                                <p className="track-page__label">Popularity</p>
                             </td>
                             <td>
-                                
+                                <p className="track-page__track-data">{track.analysis.bars.length}</p>
+                                <p className="track-page__label">Bars</p>
                             </td>
                             <td>
-                                
+                                <p className="track-page__track-data">{track.analysis.beats.length}</p>
+                                <p className="track-page__label">Beats</p>
                             </td>
                             <td>
-                                
+                                <p className="track-page__track-data">{track.analysis.sections.length}</p>
+                                <p className="track-page__label">Sections</p>
                             </td>
                             <td>
-                                
+                                <p className="track-page__track-data">{track.analysis.segments.length}</p>
+                                <p className="track-page__label">Segments</p>
                             </td>
                         </tr>
                     </tbody>
                 </table>
+            </div>
+
+            {/* Audio Features */}
+            <div className="track-page__features">
+
+                <h2>Audio Features</h2>
+
+                <Bar 
+                    data = {{
+                        labels: Object.keys(track.features),
+                        datasets: [{
+                            label: 'Features',
+                            data: Object.values(track.features),
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    }}
+                />
             </div>
         </div>
     )
