ffcece3acd617b169f7321a4d1024eb2ecc4bbd7
Michael Tobis <tobi4120@mylaurier.ca>
2021-06-10 01:12:12 -0400

    artist page styling

--- client/src/components/home/pages/artist.js
+++ client/src/components/home/pages/artist.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import useAuth from "../../useAuth";
 import Loader from "../loader";
+import UserIcon from "../page_elements/user-icon"
 
 export default function Artist(props) {
     const accessToken = useAuth(props.code)
@@ -42,23 +43,29 @@ export default function Artist(props) {
     return (
         <div className="artist-page">
             {artist.images[0]?
-                <img src={artist.images[0].url}/>:
-                <p>Image not found!</p>
+                <img className="artist-page__image" src={artist.images[0].url}/>:
+                <div className="artist-page__image artist-page__user-icon"><UserIcon /></div>
             }
-            <h1>{artist.name}</h1> 
+            <h1 className="heading-primary">{artist.name}</h1> 
 
             <div className="artist-page__stats">
-                <p>{numberWithCommas(artist.followers.total)}</p>
-                <h2>Followers</h2>
 
-                {artist.genres.map((genre, index) => {
-                    return <p key={index}>{genre}</p>
-                })}
-                <h2>Genres</h2>
+                <div className="artist-page__stats__group">
+                    <p className="artist-page__stats__stat">{numberWithCommas(artist.followers.total)}</p>
+                    <h2 className="artist-page__stats__header">Followers</h2>
+                </div>
 
-                <p>{artist.popularity}%</p>
-                <h2>Popularity</h2>
-
+                <div className="artist-page__stats__group">
+                    {artist.genres.map((genre, index) => {
+                        return <p className="artist-page__stats__stat genre" key={index}>{genre}</p>
+                    })}
+                    <h2 className="artist-page__stats__header">Genres</h2>
+                </div>
+                
+                <div className="artist-page__stats__group">
+                    <p className="artist-page__stats__stat">{artist.popularity}%</p>
+                    <h2 className="artist-page__stats__header">Popularity</h2>
+                </div>
             </div>
         </div>
     )
