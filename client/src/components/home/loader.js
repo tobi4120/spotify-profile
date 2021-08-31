31b042f92a85edee95f0ab8ee2f6de795d3e4a07
Michael Tobis <tobi4120@mylaurier.ca>
2021-08-30 20:49:25 -0400

    created loader

--- client/src/components/home/loader.js
+++ client/src/components/home/loader.js
@@ -1,3 +1,11 @@
-export default function Loader(props) {
-    return <div>Loading...</div>
+export default function Loader() {
+    return (
+        <div className="loader">
+            <div className="rectangle rect-1" />
+            <div className="rectangle rect-2" />
+            <div className="rectangle rect-3" />
+            <div className="rectangle rect-4" />
+            <div className="rectangle rect-5" />
+        </div>
+    )
 }
