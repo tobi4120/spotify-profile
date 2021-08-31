6bafe49951bd7439b692b54c7eeb2686be52f79b
Michael Tobis <tobi4120@mylaurier.ca>
2021-08-31 10:49:48 -0400

    mobile styling for first 2 pages

--- client/src/components/home/page_elements/toggle_buttons.js
+++ client/src/components/home/page_elements/toggle_buttons.js
@@ -1,12 +1,18 @@
 export default function ToggleBts(props) {
     return (
         <div className="toggle-btns">
-            <button className={props.term === "long_term"? "btn-active paragraph": "paragraph"} 
-                onClick={() => props.set_term("long_term")}>All Time</button>
-            <button className={props.term === "medium_term"? "btn-active paragraph": "paragraph"} 
-                onClick={() => props.set_term("medium_term")}>Last 6 Months</button>
-            <button className={props.term === "short_term"? "btn-active paragraph": "paragraph"}
-                onClick={() => props.set_term("short_term")}>Last 4 Weeks</button>
+            <div className="btn-container">
+                <button className={props.term === "long_term"? "btn-active paragraph": "paragraph"} 
+                    onClick={() => props.set_term("long_term")}>All Time</button>
+            </div>
+            <div className="btn-container">
+                <button className={props.term === "medium_term"? "btn-active paragraph": "paragraph"} 
+                    onClick={() => props.set_term("medium_term")}>Last 6 Months</button>
+            </div>
+            <div className="btn-container">
+                <button className={props.term === "short_term"? "btn-active paragraph": "paragraph"}
+                    onClick={() => props.set_term("short_term")}>Last 4 Weeks</button>
+            </div>
         </div>
     )
 }
