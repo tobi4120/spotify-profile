c358ac7b024888cd079f0a0f98590c7fb113a694
Michael Tobis <tobi4120@mylaurier.ca>
2021-09-05 13:15:56 -0400

    login show dialog

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -10,7 +10,7 @@ function Login() {
         <div className="login-page">
             <div className="contents">
                 <h1 className="heading-primary">Spotify Profile</h1>
-                <a className="btn-secondary" href={`https://accounts.spotify.com/authorize?client_id=71a31a9286b54db9839fd01a9d059ae1&response_type=code&redirect_uri=${WEB_URL}&scope=user-read-recently-played%20user-top-read%20playlist-read-private%20user-library-read%20user-follow-read%20user-read-email`}
+                <a className="btn-secondary" href={`https://accounts.spotify.com/authorize?client_id=71a31a9286b54db9839fd01a9d059ae1&response_type=code&redirect_uri=${WEB_URL}&scope=user-read-recently-played%20user-top-read%20playlist-read-private%20user-library-read%20user-follow-read%20user-read-email&show_dialog=true`}
                 >Log In To Spotify</a>
             </div>
         </div>
